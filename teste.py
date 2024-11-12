a1017510109fbf29fb1f7d310ee46c254ffdde74
yagodev <yagocampos11@hotmail.com>
2024-11-11 22:59:09 -0300

    limpando arquivos

--- teste.py
+++
@@ -1,9 +0,0 @@
-idade = input(str("Qual é a sua idade?"))
-
-anoNascimento = idade - 2024
-
-if anoNascimento < 2004:
-    print("Você é menor de idade")
-else:
-    print("Você é elegível")
-print(anoNascimento)
